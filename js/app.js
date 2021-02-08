68e8c4cf2d16ef9d1876461e51d66af5f50fd192
Gökçe Aktuğ <gokceeaktug@gmail.com>
2021-02-08 19:58:35 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+ const formKaydet=document.getElementById("job");
+ const mainForm=document.getElementById("main");
+ const contactForm=document.getElementById("contact");
+ const educationForm=document.getElementById("education");
+ document.getElementById("main").addEventListener("click",main);
+ document.getElementById("contact").addEventListener("click",contact);
+ document.getElementById("education").addEventListener("click",education);
+document.getElementById("submit").addEventListener("click", job);
+function formbtn()
+{
+      alert("Kayıt Başarılı");
+}
